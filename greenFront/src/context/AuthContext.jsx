5de05f582fcb918cf20805ff80bfcd76a577bfcf
sundaydavid383 <sundayudoh383@gmail.com>
2025-07-29 21:09:58 +0100

    todays commit

--- greenFront/src/context/AuthContext.jsx
+++ greenFront/src/context/AuthContext.jsx
@@ -34,7 +34,7 @@ export const AuthProvider = ({ children }) => {
     try {
       console.log("this is the form data", formData);
       const response = await axios.post(
-        `${import.meta.env.VITE_API_URL}/api/auth/register`,
+        `${import.meta.env.VITE_API_URL||'http://localhost:3000'}/api/auth/register`,
         formData,
         {
           headers: {
